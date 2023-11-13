176a119f8888c70f82862c787314f4ffc35615b0
Barbarita <89460645+BarbaraCarvajal@users.noreply.github.com>
2023-11-13 17:36:21 -0300

    Update server.js

--- server.js
+++ server.js
@@ -8,12 +8,17 @@ import cors from "cors";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productsRoutes from "./routes/productRoutes.js";
 import path from "path";
+import { fileURLToPath } from 'url';
 
 //config dotenv
 dotenv.config();
 
 //database config
 connectDB();
+
+//esmodeule fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //rest object
 const app = express();
